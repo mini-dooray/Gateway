066bee81618218ecb10c0ba6abd85a60faaee605
김관진 <cozyschooll@gmail.com>
2023-06-04 17:50:35 +0900

    Oauth 로그인 구현

--- src/main/java/com/minidooray/gateway/config/SecurityConfig.java
+++ src/main/java/com/minidooray/gateway/config/SecurityConfig.java
@@ -1,2 +1,52 @@
-package com.minidooray.gateway.config;public class SecurityConfig {
+package com.minidooray.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                    .antMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .and()
+                .oauth2Login()
+                    .clientRegistrationRepository(clientRegistrationRepository())
+                    .authorizedClientService(auth2AuthorizedClientService())
+                    .and()
+                .build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(github());
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService auth2AuthorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
+    }
+
+    private ClientRegistration github() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .userNameAttributeName("name")
+                .clientId("df5cbabe42b951bf6043")
+                .clientSecret("a071098f7eb7e894335ed593585473427fb6b812")
+                .build();
+    }
 }
